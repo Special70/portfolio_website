18b40d078676f660d778467f264ae4d35a71b5fc
Special70 <josef.urquico@gmail.com>
2023-05-21 15:44:23 +0800

    Double Checks

--- index.js
+++ index.js
@@ -2,6 +2,5 @@ let numberOfClicks = 0;
 
 document.getElementById("button1").onclick = function() {
     numberOfClicks++;
-    document.getElementById("label2").innerHTML = "You pressed the button "+numberOfClicks+" times!";
     console.log("Test "+numberOfClicks);
 }
