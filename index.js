7c59bfe8ac5bba2f277d197f164189ae690cfec8
Special70 <josef.urquico@gmail.com>
2023-05-23 22:45:10 +0800

    Smol Update
    added random crap

--- index.js
+++ index.js
@@ -2,5 +2,16 @@ let numberOfClicks = 0;
 
 document.getElementById("button1").onclick = function() {
     numberOfClicks++;
+    document.getElementById("label2").innerHTML = "You pressed the button "+numberOfClicks+" times!";
     console.log("Test "+numberOfClicks);
+}
+function add(type) {
+
+    //Create an input type dynamically.
+    var element = document.createElement("input");
+    var text = document.getElementById("textId");
+
+    //Append the element in page (in span).
+    text.appendChild(element);
+
 }
