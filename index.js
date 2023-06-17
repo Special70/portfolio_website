a5d76c4876b597a0022c6a57e1a240ee718c011b
Special70 <josef.urquico@gmail.com>
2023-06-17 14:59:45 +0800

    a

--- index.js
+++ index.js
@@ -5,13 +5,3 @@ document.getElementById("button1").onclick = function() {
     document.getElementById("label2").innerHTML = "You pressed the button "+numberOfClicks+" times!";
     console.log("Test "+numberOfClicks);
 }
-function add(type) {
-
-    //Create an input type dynamically.
-    var element = document.createElement("input");
-    var text = document.getElementById("textId");
-
-    //Append the element in page (in span).
-    text.appendChild(element);
-
-}
