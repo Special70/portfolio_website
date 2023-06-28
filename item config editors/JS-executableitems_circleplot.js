2a798060d669ebd6f900909d9893eef8d579812d
Special70 <josef.urquico@gmail.com>
2023-06-28 20:52:08 +0800

    Small fix
    realized using parseInt was a stupid idea when trying to get some values that can actually handle decimals

--- item config editors/JS-executableitems_circleplot.js
+++ item config editors/JS-executableitems_circleplot.js
@@ -3,7 +3,7 @@ function refreshButton() {
     let circleRate = document.getElementById("CircleRate").value;
     let circleSymbolX = document.getElementById("CircleSymbolX").value;
     let circleSymbolY = document.getElementById("CircleSymbolY").value;
-    let circleSymbolYExtraValue = parseInt(document.getElementById("CircleSymbolY-Add").value);
+    let circleSymbolYExtraValue = document.getElementById("CircleSymbolY-Add").value;
     let circleCommand = document.getElementById("CircleCommandBox").value;
 
     let symbolX_Regex = new RegExp(circleSymbolX, ""); 
@@ -16,16 +16,19 @@ function refreshButton() {
     let rateCounter = Math.round(100*(360/circleRate))/100;
     let rateArray = [];
     if (!(circleRate == "")) {
+
         let degreesValue = 0;
+        // Makes sure that when adding up the rateCounter everytime, it doesn't go over 360 degrees
         for (let i = 0; i < 360; i+=rateCounter) {
         degreesValue += rateCounter;
-
+        // This is just for the array stuff to display the FOR LOOP version
         rateArray.push(degreesValue);
-
+        // Offset Formula
         let xOffset = (circleRadius*Math.sin(degreesValue*-1)*Math.cos(0*-1));
         let yOffset = ((circleRadius*Math.cos(degreesValue*-1)*Math.cos(0*-1))+circleSymbolYExtraValue);
-        
+        // Replaces the current text with said symbols
         let displayCommand = circleCommand.replace(symbolX_Regex, xOffset).replace(symbolY_Regex, yOffset);
+        // Adds the text to the textarea
         document.getElementById("CircleOutputBox").innerHTML += displayCommand+"\n";
         }
     }
