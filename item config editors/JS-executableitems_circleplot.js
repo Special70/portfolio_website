1afc78023f4eea75f2a1ed29e3c9106720721f08
Special70 <josef.urquico@gmail.com>
2023-06-28 08:58:07 +0800

    a

--- item config editors/JS-executableitems_circleplot.js
+++ item config editors/JS-executableitems_circleplot.js
@@ -1,6 +1,15 @@
-let circleRadius = document.getElementById("CircleRadius").value;
+function refreshButton() {
+    let circleRadius = document.getElementById("CircleRadius").value;
+    let circleRate = document.getElementById("CircleRate").value;
+    let circleSymbolX = document.getElementById("CircleSymbolX").value;
+    let circleSymbolY = document.getElementById("CircleSymbolY").value;
+    let circleCommand = document.getElementById("CircleCommandBox").value;
 
-
+    let symbolX_Regex = new RegExp(circleSymbolX, "g"); 
+    let displayCommand = circleCommand.replace(symbolX_Regex, "");
+    document.getElementById("CircleOutputBox").innerHTML = displayCommand;
+    console.log(displayCommand+" | "+circleSymbolX);
+}
 
 function downloadCirclePlot() {
     var textToSave = "This is the text to be inserted into the file.";
@@ -10,4 +19,4 @@ function downloadCirclePlot() {
     var element = document.createElement("a");
     element.setAttribute("href", "data:text/plain;charset=utf-8,");
     element.setAttribute("download", filename);
-  }
+}
