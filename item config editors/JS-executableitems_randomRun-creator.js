767a8fddcdad39bca54135f5bf10fe19dacbe764
Josef Miko <73434242+Special70@users.noreply.github.com>
2023-07-23 21:39:32 +0800

    b
    b

--- item config editors/JS-executableitems_randomRun-creator.js
+++ item config editors/JS-executableitems_randomRun-creator.js
@@ -1,17 +1,32 @@
-const cmdAdderButton = document.getElementById("RandomRun-ADD-CMD-FIELD");
+document.body.onload = createNewTextArea()
+const frameBoxVar = document.getElementById("frameBox");
 
-cmdAdderButton.addEventListener("click", function() {
+function createNewTextArea() {
     console.log("test");
     const randomRunFrame = document.getElementById("RandomRunFrame");
     // Creates extra textareas
     const newTextArea = document.createElement("textarea");
-    newTextArea.setAttribute("style", "resize: none; width: 100%; border: solid 2px darkorange");
-
-    const newNumberInputBox = document.createElement("input");
-    newNumberInputBox.setAttribute("type","number");
+    newTextArea.setAttribute("style", "width: 100%; border: solid 2px darkorange");
 
     const newNextLine = document.createElement("br");
 
-    randomRunFrame.appendChild(newNextLine);
-    randomRunFrame.appendChild(newTextArea);
+    const newTextLine = document.createElement("div");
+      newTextLine.setAttribute("style","color: darkorange");
+      const newNumberInputBox = document.createElement("input");
+      newNumberInputBox.setAttribute("type","number");
+      newNumberInputBox.setAttribute("style","width: 10%; border: solid 2px darkorange");
+      newNumberInputBox.setAttribute("value","0");
+
+      const text001 = document.createTextNode("  RNG Chance:");
+      newTextLine.append(newNumberInputBox, text001);
+
+    randomRunFrame.append(newNextLine, newTextLine, newTextArea);
+}
+
+
+const checkForInputs = frameBoxVar.querySelectorAll("input");
+
+checkForInputs.forEach(inputAction => {
+    inputAction.innerHTML = 0;
 })
+
