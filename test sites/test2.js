b43b36f4cb6f06101aaddd9c626a39ccae6d6daf
Special70 <josef.urquico@gmail.com>
2023-07-25 11:24:36 +0800

    made the visibility button-type

--- test sites/test2.js
+++ test sites/test2.js
@@ -1,16 +1,6 @@
-document.body.onload = addElement;
-
-function addElement() {
-  // create a new div element
-  const newDiv = document.createElement("div");
-
-  // and give it some content
-  const newContent = document.createTextNode("Hi there and greetings!");
-
-  // add the text node to the newly created div
-  newDiv.appendChild(newContent);
-
-  // add the newly created element and its content into the DOM
-  const currentDiv = document.getElementById("div1");
-  document.body.insertBefore(newDiv, currentDiv);
-}
+const targetDiv = document.getElementById("third");
+const btn = document.getElementById("toggle");
+btn.onclick = function () {
+    targetDiv.setAttribute("style","display: none");
+    console.log("wawa");
+};
