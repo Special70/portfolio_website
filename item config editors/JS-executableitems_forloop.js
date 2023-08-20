be18833fbfb2fec0333350588b7abe153a697714
Special70 <josef.urquico@gmail.com>
2023-08-20 11:08:33 +0800

    FOR LOOP editor to support float
    Changed parseInt() to parseFloat() to accept decimals

--- item config editors/JS-executableitems_forloop.js
+++ item config editors/JS-executableitems_forloop.js
@@ -7,13 +7,21 @@ forLoopButton.addEventListener("click", function() {
     let incrementValue = document.getElementById("ForLoop_IncrementValue").value;
     let incrementAmount = document.getElementById("ForLoop_IncrementAmount").value;
 
-    
+    /*
     if (numberRegex.test(startValue) && numberRegex.test(incrementValue)) {
         startValue = parseInt(startValue);
         incrementValue = parseInt(incrementValue);
     }
+    */
+    try {
+        startValue = parseFloat(startValue);
+        incrementValue = parseFloat(incrementValue);
+    }
+    catch (err) {
 
-    let loopValue = startValue;
+    }
+
+    let loopValue = (startValue);
     let forLoopArray = [];
 
     for (let i = 0; i < incrementAmount; i++) {
